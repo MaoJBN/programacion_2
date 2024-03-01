f7c8f010946603d355132e00fbe85a0b106aa569
MaoJBN <mjbecerran@ufpso.edu.co>
2024-03-01 11:52:27 -0500

    class 1.0.1

--- project1/src/main/java/com/mycompany/project1/Project1.java
+++ project1/src/main/java/com/mycompany/project1/Project1.java
@@ -4,6 +4,6 @@ public class Project1 {
 
     public static void main(String[] args) {
         Character homero = new Character("Homero","Inspector",38,"Doooooh");
-        System.out.println(homero.age);
+        System.out.println(homero.getOccupation());
     } 
 }
