8f56c39255e19998a81d32a2d53c021860f22166
MaoJBN <mjbecerran@ufpso.edu.co>
2024-03-01 11:31:42 -0500

    class 1

--- project1/src/main/java/com/mycompany/project1/Project1.java
+++ project1/src/main/java/com/mycompany/project1/Project1.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.project1;
 
-/**
- *
- * @author SCIS2PC03
- */
 public class Project1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        Character homero = new Character("Homero","Inspector",38,"Doooooh");
+        System.out.println(homero.age);
+    } 
 }
